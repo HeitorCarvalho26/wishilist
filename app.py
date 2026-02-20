e5cac70006f373bbdf277552f15c631e5b306609
HeitorCarvalho26 <heitorcmiguel@gmail.com>
2026-02-20 16:49:15 -0300

    feat: Finalizando o projeto

--- app.py
+++ app.py
@@ -1,30 +1,54 @@
-from flask import Flask, render_template, request, redirect, url_for
-from models.obra import Obra
+
+
+from flask import Flask, redirect, render_template, request, url_for
+from models.database import init_db
+from models.lista import Atividade
 
 app = Flask(__name__)
 
+init_db()
+
 @app.route('/')
 def home():
-    return render_template('home.html', titulo = "Lista de desejos")
+    return render_template('home.html', titulo='Home')
 
-@app.route('/wishlist', methods=['GET', 'POST'])
-def list():
-    obra = None
+@app.route('/lista', methods=['GET','POST'])
+def atividade():
+    atividades = None
 
     if request.method == 'POST':
-        titulo_obra = request.form['titulo_obra']
-        indicacao = request.form['indicacao']
-        obra = Obra(titulo_obra, indicacao)
-        obra.salvar_obra()
+        titulo_atividade = request.form['titulo-atividade']
+        tipo_de_atividade = request.form['tipo_de_atividade']
+        indicado_por = request.form['indicado_por']
+    
+        atividade = Atividade(titulo_atividade, tipo_de_atividade,indicado_por)
+        atividade.salvar_atividade()
 
-    obras = Obra.obter_obra()
-    return render_template('wishlist.html', titulo='Lista', obra=obras)
+    atividades = Atividade.obter_atividades()
+    return render_template('lista.html', titulo='Sua Lista de Desejos', atividades=atividades)
 
-@app.route('/update/<int:idObra>', methods = ['GET', 'POST'])
-def update(idObra):
+@app.route('/delete/<int:idAtividade>')
+def delete(idAtividade):
+    atividade = Atividade.id(idAtividade)
+    atividade.excluir_atividade()
+    # return render_template('agenda.html', titulo="Agenda",
+    # tarefa=tarefas)
+    return redirect(url_for('atividade'))
+
+@app.route('/update/<int:idAtividade>', methods=['GET', 'POST']) 
+def update(idAtividade):
+    atividades = None
+
     if request.method == 'POST':
-        titulo = request.form['titulo-obra']
-        indic = request.form['indicacao']
-        obra = Obra(titulo, indic, idObra)
-        obra.atualizar_obra()
-        return redirect(url_for)
+        titulo_atividade = request.form['titulo-atividade']
+        tipo_de_atividade = request.form['tipo_de_atividade']
+        atividade = Atividade(titulo_atividade, tipo_de_atividade, idAtividade)
+        atividade.atualizar_atividade()
+
+    atividades = Atividade.obter_atividades()
+    atividade_selecionada = Atividade.id(idAtividade)
+    return render_template('lista.html', titulo=f'Editando a atividade ID: {idAtividade}',atividade_selecionada=atividade_selecionada, atividades=atividades)
+
+@app.route('/ola')
+def ola_mundo():
+    return "Olá, Mundo!"
--- models/database.py
+++ models/database.py
@@ -1,60 +1,71 @@
-from types import TracebackType
-from dotenv import load_dotenv
+
 from sqlite3 import Connection, connect, Cursor
 from typing import Any, Optional, Self, Type
+from types import TracebackType
+from dotenv import load_dotenv
 import traceback
 import os
 
-load_dotenv()
-DB_PATH = os.getenv('DATABASE', './data/tarefa.sqlite3')
+load_dotenv() # Procura um arquivo .env com variáveis 
+DB_PATH = os.getenv('DATABASE', './data/lista.sqlite3')
 
-def init_db(db_name: str =DB_PATH) -> None:
+def init_db(db_name: str = DB_PATH):
     with connect(db_name) as conn:
         conn.execute("""
-        CREATE TABLE IF NOT EXITS tarefas (
+        CREATE TABLE IF NOT EXISTS atividades (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            titulo_obra TEXT NOT NULL,
-            indicacao TEXT
-            );
-""")
-        
+            titulo_atividade TEXT NOT NULL,
+            tipo_de_atividade TEXT,
+            indicado_por TEXT      
+        );
+        """)
+
 class Database:
     """
-        Classe que gerencia conexões e operações com um banco de dados SQLite. Utiliza o protocolo de gerenciamento de contexto para garantir que a conexão seja encerrada corretamente.
+        Classe que regencia conexões e operações com o banco de dados SQLitre. Utiliza o protocolo de gerenciamento de contxtos para garantir qua a conexão seja encerrada corretamente
     """
-    def __init__(self, db_name: str =DB_PATH) -> None:
+    def __init__(self, db_name: str = DB_PATH) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
         self.executar("""
-            CREATE TABLE IF NOR EXISTS obra(id INTEGER PRIMARY KEY AUTOINCREMENT, titulo_obra TEXT NOT NULL, indicacao TEXT)""")
-        
-    def executar(self, query: str, params: tuple =()) -> list[Any]:
+       CREATE TABLE IF NOT EXISTS atividades (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            titulo_atividade TEXT NOT NULL,
+            tipo_de_atividade TEXT,
+            indicado_por TEXT      
+        );
+        """)
+
+    def executar(self, query: str, params: tuple = ()) -> Cursor:
+        self.cursor.execute(query, params)
+        self.connection.commit()
+        return self.cursor
+
+    def buscar_tudo(self, query: str, params: tuple = ()) -> list[Any]:
         self.cursor.execute(query, params)
         return self.cursor.fetchall()
-    
-    def buscar_tudo(self, query: str, params: tuple = ()) -> list[Any]:
-         self.cursor.execute(query, params)
-         return self.cursor.fetchall()
-    
+
     def close(self) -> None:
         self.connection.close()
 
     # Métodos para o gerenciamento de contexto
-    # Método de entrada no contexto
+
+    # Método de entrada do contexto
     def __enter__(self) -> Self:
         return self
 
-    # metodo de saída do contexto
-    def __exit__(self,
-                exc_type: Optional[Type[BaseException]],
-                exc_value: Optional[BaseException],
-                tb: Optional[TracebackType]) -> None:
-        
-                    if exc_type is not None:
-                        print('Exceção capturada no contexto:')
-                        print(f'Tipo: {exc_type.__name__}')
-                        print(f'Mensagem: {exc_value}')
-                        print('Traceback completo:')
-                        traceback.print_tb(tb)
+    # Método de saída do contexto
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        tb: Optional[TracebackType],
+    ) -> None:
+        if exc_type is not None:
+            print("Exceção capiturar no contexto: ")
+            print(f"Tipo: {exc_type.__name__}")
+            print(f"Mensagem: {exc_value}")
+            print("Traceback completo:")
+            traceback.print_tb(tb)
 
-                    self.close()
+        self.close()
